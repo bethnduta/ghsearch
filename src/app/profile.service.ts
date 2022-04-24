aba29253c80f128c4fc8ea94b3a006452d002fe8
beth nduta <bethnduta05@gmail.com>
2022-04-24 23:57:41 +0300

    modified service and profile

--- src/app/profile.service.ts
+++ src/app/profile.service.ts
@@ -1,34 +1,43 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { map } from 'rxjs';
 import 'rxjs/add/operator/map';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
-  private username:string | undefined;
-  private clientid = 'da0bc7b80a8cf72abac4';
-  private clientSecret = 'd7e6cb289367824141c101eb6259f56779713b96';
-  client_id: string | undefined;
-  client_secret: string | undefined;
+  private username:string = 'bethnduta';
+  private client_id:string = 'da0bc7b80a8cf72abac4';
+  private client_Secret:string = 'd7e6cb289367824141c101eb6259f56779713b96';
+  // client_id: string | undefined;
+  // client_secret: string | undefined;
   private _http: any;
   constructor(private http:HttpClient) {
     console.log("service is now ready!");
     this.username = 'bethnduta';
     console.log("service is now ready!");
-    this.username = 'bethnduta';
+
   }
-  
+
   getUser(){
-    return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_secret)
+    return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_Secret)
         .map((res: { json: () => any; }) => res.json());
 }
+  
+//   getUser(){
+//     return this._http.get('https://api.github.com/users/'+this.username+'?client_id='+this.client_id+'&client_secret='+this.client_Secret)
+//         .map((res: { json: () => any; }) => res.json());
+// }
 
 getRepos(){
-  return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_secret)
-      .map((res: { json: () => any; }) => res.json());
-}
+        return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_Secret)
+            .map((res: { json: () => any; }) => res.json());
+    }
+
+// getRepos(){
+//   return this._http.get('https://api.github.com/users/'+this.username+'/repos?client_id='+this.client_id+'&client_secret='+this.client_Secret)
+//       .map((res: { json: () => any; }) => res.json());
+// }
 
 updateUsername(username:string){
   this.username = username;
--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,28 +8,40 @@ import { ProfileService } from '../profile.service';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-  profile: any[] = [];
+  user: any[] = [];
   repos: any[] = [];
   username !: string ;
   constructor(private profileService: ProfileService) {
-    this.profileService.getProfileInfo().subscribe((profile: any) => {
+    this.profileService.getUser().subscribe((user: any) => {
       console.log(profile);
-      this.profile = profile;
+      this.user = user;
     });
 
-    this.profileService.getProfileRepos().subscribe((repos: any[]) => {
+    this.profileService.getRepos().subscribe((repos: any[]) => {
       console.log(repos);
       this.repos = repos;
     });
 
 
   }
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
   
   findProfile() {
-    this.profileService.updateProfile(this.username);
+    this.profileService.updateUsername(this.username);
+        
+        this.profileService.getUser().subscribe((_users: any) => {
+            //console.log(user);
+            this.user = _users;
+        });
+        
+        this.profileService.getRepos().subscribe((repos: any[]) => {
+            this.repos = repos;
+        });
+    }
   }
-
-  ngOnInit(): void {
-  }
-
+function ngOnInit() {
+  throw new Error('Function not implemented.');
 }
+
